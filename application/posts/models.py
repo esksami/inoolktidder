01792bf466e5ae46ad623dbf0516dce4bc366a78
Your Name <eskelinenn.sami@gmail.com>
2019-11-07 23:11:48 +0200

    Simple initialization

---
+++ application/posts/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
+    title = db.Column(db.String(512), nullable=False)
+    content = db.Column(db.String(8192), nullable=False)
+    likes = db.Column(db.Integer, default=0)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
