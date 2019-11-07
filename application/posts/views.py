33c001ee24689b5dbc2f97e92bcaab765ee0c78a
Your Name <eskelinenn.sami@gmail.com>
2019-11-07 23:41:05 +0200

    User can edit posts

--- application/posts/views.py
+++ application/posts/views.py
@@ -10,6 +10,22 @@ def posts_index():
 def posts_form():
     return render_template("posts/new.html")
 
+@app.route("/posts/edit/<post_id>/")
+def posts_edit_form(post_id):
+    return render_template("posts/edit.html", post=Post.query.get(post_id))
+
+@app.route("/posts/edit/<post_id>/", methods=["POST"])
+def posts_edit(post_id):
+    title = request.form.get("title")
+    content = request.form.get("content")
+
+    post = Post.query.get(post_id)
+    post.title = title
+    post.content = content
+    db.session().commit()
+  
+    return redirect(url_for("posts_details", post_id=post_id))
+
 @app.route("/posts/<post_id>/")
 def posts_details(post_id):
     return render_template("posts/details.html", post=Post.query.get(post_id))
