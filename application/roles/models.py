a5decd58a914cfaea16b71ad7322c117e69b2894
Your Name <eskelinenn.sami@gmail.com>
2019-12-06 18:09:16 +0200

    Use custom base class and session_scope

--- application/roles/models.py
+++ application/roles/models.py
@@ -1,29 +1,32 @@
 from application import db
+from application.models import Base
+from application.utils import session_scope
 
-from sqlalchemy.orm import relationship
 from sqlalchemy import event
+from sqlalchemy.orm import relationship
 
 
-class Role(db.Model):  
-    id = db.Column(db.Integer, primary_key=True)
-    
+class Role(Base):      
     name = db.Column(db.String(128), unique=True)
 
-class UserRole(db.Model):  
-    id = db.Column(db.Integer, primary_key=True)
-    
+class UserRole(Base):      
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'),
                         nullable=False)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False) 
 
-    user = relationship("User", backref="UserRole")
-    role = relationship("Role", backref="UserRole")
+    role = relationship("Role", backref="user_role")
+    user = relationship("User", backref="user_role")
+
 
 @event.listens_for(Role.__table__, 'after_create')
 def insert_initial_values(*args, **kwargs):
-    db.session.add(Role(name='APPROVED'))
-    db.session.add(Role(name='BANNED'))
-    db.session.add(Role(name='ADMIN'))
-    db.session.add(Role(name='USER'))
-    db.session.commit()
+    ROLE_NAMES = ['APPROVED', 'BANNED', 'MODERATOR', 'USER']
+
+    with session_scope() as session:
+        roles = Role.query.filter(Role.name.in_(['APPROVED', 'USER'])).all()
+        session.bulk_save_objects(
+            [Role(name=role_name) for role_name in ROLE_NAMES]
+        )
+
+        session.commit()
