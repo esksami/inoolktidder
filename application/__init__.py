ae26acef1f441ee9f16ffc8452dff542a68158da
Your Name <eskelinenn.sami@gmail.com>
2019-12-07 02:23:00 +0200

    Import user just before implementing load_user

--- application/__init__.py
+++ application/__init__.py
@@ -24,6 +24,9 @@ login_manager.init_app(app)
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
+
+from application.auth.models import User
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
